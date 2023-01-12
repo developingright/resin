511dccd4d033cd275a0407f25db82ffb5195df34
developingright <mshaheer2003@gmail.com>
2023-01-12 18:25:03 +0530

    Added react router and created home.js component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
+import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Router> 
+        <Routes>
+          <Route path='/' element={<Home/>} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <header className="masthead">
+            <div className="container px-5">
+                <div className="row gx-5 align-items-center">
+                    <div className="col-lg-6">
+                        <div className="mb-5 mb-lg-0 text-center text-lg-start">
+                            <h1 className="display-1 lh-1 mb-3">Get your research intern</h1>
+                            <p className="lead fw-normal text-muted mb-5">Perfect platform for students to get research intern opportunities</p>
+                            <div className="d-flex flex-column flex-lg-row align-items-center">
+                                <button type="button" className="btn btn-outline-primary">Know more</button>
+                                <a href="#!"><img className="app-badge" src="assets/img/app-store-badge.svg" alt="..." /></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-6">
+                       <h2 className='mb-5 text-center'> Hello world</h2>
+                       
+                    </div>
+                </div>
+            </div>
+        </header>
+  )
+}
