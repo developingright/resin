c5196b3842269a67caa26b44251cc2048e00b3c4
developingright <mshaheer2003@gmail.com>
2023-01-31 11:16:53 +0530

    changed the signup page look, added radio button for account type, divided the code into components for better readability, step1 for signup completed.

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,59 +1,98 @@
 import React from 'react'
 import InputField from '../Utils.js';
 
-const Form = () => {
+const handleChange = () =>{
+  var radios = document.querySelectorAll('input[name="radio"]');
+  for (var i = 0 ; i < radios.length; i++){
+    var elem = document.querySelector(`label[for="${radios[i].id}"]`);
+    if (radios[i].checked){
+      elem.className+=' checked';
+    }
+    else{
+      elem.classList.remove('checked');
+    }
+  } 
+}
+
+const RadioComp = ()  =>{
+  return(
+    <div className="d-flex flex-row signup-check">
+        <div className="col-md-5">
+          <label className='checks' htmlFor="box">
+            <input type="radio"  id="box" onChange={handleChange} name = 'radio'/>
+            <p>Professor</p>
+          </label>
+        </div>
+        <div className="col-md-4">
+        <label className='checks' htmlFor="box2">
+            <input type="radio"  id="box2" name='radio' onChange={handleChange}/>
+            <p>Student</p>
+          </label>
+        </div>
+    </div>
+  );
+}
+
+const Form1 = () => {
   const inputs = 
     [ {id:"form3Example1c",text:"Your Name",type:"text"},
-      {id:"form3Example3c",text:"Your Email",type:"email"},
+      {id:"form3Example3c",text:"Email",type:"email"},
       {id:"form3Example4c",text:"Password",type:"password"},
       {id:"form3Example5c",text:"Repeat your password",type:"password"}
     ]
   return(
-    <form className="mx-1 mx-md-4">
-      {inputs.map(x =>
-          <div className="d-flex flex-row align-items-center mb-4" key = {x.text}>
-            <InputField 
-              formclassName={"form-outline flex-fill mb-0"} 
-              type={x.type} 
-              id={x.name} 
-              inputClass={"form-control"} 
-              labelClass={"form-label"} 
-              labelFor={x.name}
-              labelText={x.text}
-            />
-          </div>
-        )}
-      <div className="form-check d-flex justify-content-center mb-5">
-        <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
-        <label className="form-check-label" htmlFor="form2Example3">
-          I agree all statements in <a href="#!">Terms of service</a>
-        </label>
+      <form className="mx-1 mx-md-4 sign-up-form">
+        {inputs.map(x =>
+            <div className="d-flex flex-row align-items-center mb-4" key = {x.text}>
+              <InputField 
+                formclassName={"form-outline flex-fill mb-0"} 
+                type={x.type} 
+                id={x.id} 
+                inputClass={"form-control sign-up-input"} 
+                labelClass={"form-label sign-up-label"} 
+                labelFor={x.id}
+                labelText={x.text}
+              />
+            </div>
+          )}
+        <div className="form-check d-flex justify-content-center mb-5">
+          <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
+          <label className="form-check-label" htmlFor="form2Example3">
+            I agree all statements in <a href="#! ">Terms of service</a>
+          </label>
+        </div>
+        <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+          <button type="button" className="next-button">&#8250;</button>
+        </div>
+    </form>
+  );
+}
+
+const Step1 = () => {
+  return(
+    <>
+      <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+        <p className="text-center h1 fw-bold signup-title">Sign up</p>
+        <RadioComp />
+        <Form1 />
       </div>
-      <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-        <button type="button" className="btn btn-primary btn-lg">Register</button>
+      <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2 signup-banner">
       </div>
-  </form>
+    </>
   );
 } 
 
+
 export default function SignUp() {
-  const form_img="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp";
   return (
     <section className="signin-section" style={{"backgroundColor": "eee"}}>
       <div className="container h-100">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col-lg-12 col-xl-11">
-            <div className="card text-black" style={{"borderRadius": "25px"}}>
-              <div className="card-body p-md-5">
+          <div className="card text-black" style={{"borderRadius": "25px"}}>
+              <div className="card-body ">
                 <div className="row justify-content-center">
-                  <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-                    <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
-                    <Form />
-                  </div>
-                  <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-                    <img src={form_img}
-                      className="img-fluid" alt="sample"/>
-                  </div>
+                    <Step1 />
                 </div>
               </div>
             </div>
--- src/Utils.js
+++ src/Utils.js
@@ -1,8 +1,8 @@
 const InputField = (props) =>{
     return(
         <div className={props.formClass}>
+            <label className={props.labelClass} htmlFor={props.labelFor} value={props.value}>{props.labelText}</label>
             <input type={props.type} id={props.id} className={props.inputClass} />
-            <label className={props.labelClass} htmlFor={props.labelFor}>{props.labelText}</label>
         </div>  
     );
 }
